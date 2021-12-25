a56641a2a7fd40d6f572a0bc6d9f47a5ab4a6bfc
KinnukumarSingh <96306300+KinnukumarSingh@users.noreply.github.com>
2021-12-25 20:04:37 +0530

    Greater number from the 3 number

---
+++ Firstprogram.java
@@ -0,0 +1,22 @@
+package GreaterNumber;
+
+public class Firstprogram {
+
+
+	public static void main(String[] args) {
+		int i = 10;
+		int j = 20;
+		int k = 40;
+		if(i>j && i>k) {
+			System.out.print(i);
+		}
+		else if(j>i && j>k) {
+			System.out.print(j);
+		}
+		else {
+			System.out.print(k);
+		}
+
+	}
+
+}
